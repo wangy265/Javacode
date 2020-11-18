5a4fe6c04a2aa611d504d7e45412ac92c132e55a
yueqing,wang <yueqing.wang.partner@decathlon.com>
2020-11-18 15:40:01 +0800

    init jedis demo

---
+++ jredis/src/main/java/jredis/RedisConfig.java
@@ -0,0 +1,46 @@
+package jredis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class RedisConfig {
+	
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Value("${redis.timeout}")
+    private int timeout;
+
+    @Value("${redis.maxIdle}")
+    private int maxIdle;
+
+    @Value("${redis.maxWaitMillis}")
+    private int maxWaitMillis;
+
+    @Value("${redis.blockWhenExhausted}")
+    private Boolean blockWhenExhausted;
+
+    @Value("${redis.JmxEnabled}")
+    private Boolean jmxEnabled;
+    
+    @Bean
+    public JedisPool jedisPoolFactory() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
+        jedisPoolConfig.setJmxEnabled(jmxEnabled);
+        return new JedisPool(jedisPoolConfig, host, port, timeout);
+    }
+
+}
